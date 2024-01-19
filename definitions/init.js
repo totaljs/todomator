9136b9660e55c51a8009fb6a6d8692d3ec06ba13
Peter Širka <petersirka@gmail.com>
2024-01-19 11:02:14 +0100

    Updated version.

--- definitions/init.js
+++ definitions/init.js
@@ -1,7 +1,7 @@
 require('querybuilderpg').init('default', CONF.database, CONF.pooling || 1, ERROR('DB'));
 
 CONF.$customtitles = true;
-CONF.version = '1.1';
+CONF.version = '1.2';
 
 // UI components
 ON('ready', function() {
