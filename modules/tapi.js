dd8caf3416e07fa5056af395a2569c4181448b11
Peter Širka <petersirka@gmail.com>
2024-02-17 11:14:37 +0100

    Fixed TAPI module.

--- modules/tapi.js
+++ modules/tapi.js
@@ -9,41 +9,14 @@ function api($) {
 		return;
 	}
 
-	var items = [];
-	var output = [];
+	var routes = F.sourcemap().routes;
+	var response = [];
 
-	for (let key in F.actions) {
-		let action = F.actions[key];
-		if (action.public)
-			items.push({ action: key, schema: key, icon: action.icon, name: action.name, params: action.params, input: action.input, ouptut: action.ouptut, query: action.query, summary: action.summary });
+	for (var item of routes) {
+		if (item.public && item.url === CONF.$api)
+			response.push({ id: item.id, name: item.name, summary: item.summary, params: item.params, query: item.query, input: item.input, output: item.output });
 	}
 
-	for (let route of F.routes.routes) {
-
-		if (!route.api)
-			continue;
-
-		var actions = route.api;
-		for (var b in actions) {
-			var action = actions[b];
-			for (var i = 0; i < items.length; i++) {
-				var m = items[i];
-
-				if (CONF.tapiendpoint && action.url !== CONF.tapiendpoint)
-					continue;
-
-				if (action.actions.indexOf(m.action) !== -1) {
-					m.id = action.name;
-					m.url = $.hostname(route.url2);
-					m.action = undefined;
-					m.schema = undefined;
-					output.push(m);
-					break;
-				}
-			}
-		}
-	}
-
-	TEMP.TAPI = output;
-	$.json(output);
+	TEMP.TAPI = response;
+	$.json(TEMP.TAPI);
 }
