080bda02c155c874f81799edf8ab0aa628948638
petersirka <petersirka@gmail.com>
2024-05-22 09:34:16 +0200

    Updated UI components.

--- definitions/init.js
+++ definitions/init.js
@@ -7,7 +7,7 @@ CONF.version = '1.2';
 ON('ready', function() {
 
 	// Componentator
-	COMPONENTATOR('ui', 'exec,menu,input,columns,loading,idletime,extend,icons,floatingbox,autofill,rawinput,edit,errorhandler,floatinginput,approve,colorpicker,virtualwire,breadcrumb,page,importer,navlayout,viewbox,enter,validate,selection,searchinput,selected,box,tangular-color,tangular-filesize,intranetcss,notify,tabmenu,ready,iframepreview,preview,datepicker,paper,timepicker,dropfiles,locale,empty,miniform,fileuploader,websocket,search,title,aselected,directory,clipboard,nativenotifications,sounds,markdown,clipboardimage,shortcuts,faviconunread,filesaver,info,inlinedatepicker,uibuilder,uistudio,prompt', true);
+	COMPONENTATOR('ui', 'exec,menu,input,columns,idletime,loading,extend,icons,floatingbox,autofill,rawinput,edit,errorhandler,floatinginput,approve,colorpicker,virtualwire,breadcrumb,page,importer,navlayout,viewbox,enter,validate,selection,searchinput,selected,box,tangular-color,tangular-filesize,intranetcss,notify,tabmenu,ready,iframepreview,preview,datepicker,paper,timepicker,dropfiles,locale,empty,miniform,fileuploader,websocket,search,title,aselected,directory,clipboard,nativenotifications,sounds,markdown,clipboardimage,shortcuts,faviconunread,filesaver,info,inlinedatepicker,uibuilder,uistudio,prompt', true);
 
 });
 
